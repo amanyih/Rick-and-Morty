f368a76a17916b0a5acc7889efb7c4e27d9a7c25
amanuel <amanuelyihunie27@gmail.com>
2023-03-15 15:57:48 +0300

    Intializing Project

---
+++ rick-and-morty/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import logo1 from "./Assests/logo1.png";
+import logo2 from "./Assests/logo2.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo1} alt="" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
