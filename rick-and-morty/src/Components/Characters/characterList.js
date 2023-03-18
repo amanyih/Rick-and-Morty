1e3ec3b10d169dd1b394ce75fb16acb72bde158c
amanuel <amanuelyihunie27@gmail.com>
2023-03-18 22:41:03 +0300

    add previous charachters page

--- rick-and-morty/src/Components/Characters/characterList.js
+++ rick-and-morty/src/Components/Characters/characterList.js
@@ -2,10 +2,12 @@ import Character from "./character";
 
 import placeholder from "../../Assests/placeholder.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Filter from "./Filter";
 
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
 import ReactPaginate from "react-paginate";
+import InputField from "../UI/Input/input";
 
 const CharacterList = () => {
   const [chars, setChars] = useState(null);
@@ -20,51 +22,54 @@ const CharacterList = () => {
       });
   }, []);
   const handlePageChange = async (page) => {
-    // setChars(null);
     const p = page.selected + 1;
 
     const dataFromServer = await fetch(
       `https://rickandmortyapi.com/api/character/?page=${p}`
     );
     const jsonData = await dataFromServer.json();
-    // console.log(jsonData.results);
     setChars(jsonData.results);
-    // console.log();
-    // console.log(page);
   };
   return (
-    <div className=" flex flex-col alig mt-12 p-12">
-      <h1 className=" text-7xl text-yellow-100">Characters</h1>
-      <div className="flex flex-wrap bg-lighCharcoal rounded-2xl mt-12 p-12 justify-evenly">
+    <div className=" flex justify-between align-middle m-4">
+      <div className="w-1/4 flex justify-center align-middle">
+        <Filter></Filter>
+      </div>
+      <div className=" flex flex-col alig w-8/12">
+        <h1 className=" text-7xl text-yellow-100">Characters</h1>
+        <InputField></InputField>
+
+        <div className="flex flex-wrap bg-lighCharcoal rounded-2xl mt-12 p-12 justify-evenly">
+          {chars == null ? (
+            <FontAwesomeIcon
+              icon={faSpinner}
+              className="animate-spin text-white border"
+            ></FontAwesomeIcon>
+          ) : (
+            chars.map((element) => (
+              <Character
+                name={element.name}
+                id={element.id}
+                status={element.status}
+                img={element.image}
+                //   species={element.species}
+              ></Character>
+            ))
+          )}
+        </div>
         {chars == null ? (
-          <FontAwesomeIcon
-            icon={faSpinner}
-            className="animate-spin text-white border"
-          ></FontAwesomeIcon>
+          ""
         ) : (
-          chars.map((element) => (
-            <Character
-              name={element.name}
-              id={element.id}
-              status={element.status}
-              img={element.image}
-              //   species={element.species}
-            ></Character>
-          ))
+          <ReactPaginate
+            pageCount={42}
+            containerClassName="flex mx-auto mt-6"
+            pageClassName="ml-6 border-2 border-blue-800 py-1 px-2 "
+            nextClassName="ml-6"
+            onPageChange={handlePageChange}
+            activeClassName="bg-lightBlue"
+          ></ReactPaginate>
         )}
       </div>
-      {chars == null ? (
-        ""
-      ) : (
-        <ReactPaginate
-          pageCount={42}
-          containerClassName="flex mx-auto mt-6"
-          pageClassName="ml-6 border-2 border-blue-800 py-1 px-2 "
-          nextClassName="ml-6"
-          onPageChange={handlePageChange}
-          activeClassName="bg-lightBlue"
-        ></ReactPaginate>
-      )}
     </div>
   );
 };
