9d29ce53fa4b0ae53e9bd8c00ba9aed89bc52e19
amanuel <amanuelyihunie27@gmail.com>
2023-08-10 17:24:01 +0300

    hard reset

--- src/App.js
+++ src/App.js
@@ -1,49 +1,7 @@
 import "./App.css";
-// import { faGit } from "@fortawesome/free-regular-svg-icons";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-import RootLayout from "./Layout/rootLayOut";
-import Home from "./Pages/Home";
-import CharacterList from "./Components/Characters/characterList";
-import CharactersLayout from "./Layout/CharactersLayout";
-// import Location from "./Components/Locations/location";
-// import Episode from "./Components/Episodes/episodes";
-import CharacterDetail from "./Components/Characters/characterDetail";
-// import { CharacterDetailLoader } from "./Components/Characters/characterDetail";
-import CharacterNotFound from "./Components/UI/Error/error";
-import EpisodesLayout from "./Layout/EpisodesLayout";
-import EpisodesList from "./Components/Episodes/episodesList";
-import LocationLayout from "./Layout/LocationLayout";
-import LocationList from "./Components/Locations/LocationsList";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/characters" element={<CharactersLayout />}>
-        <Route index element={<CharacterList />}></Route>
-        <Route
-          path=":id"
-          errorElement={<CharacterNotFound />}
-          element={<CharacterDetail />}
-        ></Route>
-      </Route>
-      <Route path="/locations" element={<LocationLayout />}>
-        <Route index element={<LocationList />}></Route>
-      </Route>
-      <Route path="/episodes" element={<EpisodesLayout />}>
-        <Route index element={<EpisodesList />}></Route>
-      </Route>
-    </Route>
-  )
-);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return <div>Hello Rick and Morty</div>;
 }
 
 export default App;
--- src/Components/UI/Card/card.js
+++
@@ -1,7 +0,0 @@
-const Card = (props) => {
-  return (
-    <div className=" rounded-lg p-2 overflow-hidden">{props.children}</div>
-  );
-};
-
-export default Card;
